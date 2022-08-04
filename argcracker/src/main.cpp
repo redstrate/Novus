ea4a056815c52ec1b7976befa8b113b86b54f669
Joshua Goins <josh@redstrate.com>
2022-08-04 11:32:12 -0400

    Use blowfish implementation from physis

--- argcracker/src/main.cpp
+++ argcracker/src/main.cpp
@@ -1,7 +1,7 @@
 #include <fmt/core.h>
 #include <QString>
 
-#include "blowfish.h"
+#include <physis.hpp>
 
 // from xivdev
 static char ChecksumTable[] = {
@@ -24,8 +24,19 @@ inline QString encryptGameArg(QString arg) {
     char buffer[9] = {};
     sprintf(buffer, "%08x", key);
 
-    Blowfish session(QByteArray(buffer, 8));
-    QByteArray encryptedArg = session.Encrypt((QString(" /T =%1").arg(ticks) + arg).toUtf8());
+    Blowfish* session = physis_blowfish_initialize(reinterpret_cast<uint8_t *>(buffer), 9);
+
+    uint8_t* out_data = nullptr;
+    uint32_t out_size = 0;
+
+    QByteArray toEncrypt = (QString(" /T =%1").arg(ticks) + arg).toUtf8();
+
+    physis_blowfish_encrypt(session,
+                            reinterpret_cast<uint8_t*>(toEncrypt.data()), toEncrypt.size(), &out_data, &out_size);
+
+    QByteArray encryptedArg = QByteArray::fromRawData(
+        reinterpret_cast<const char*>(out_data), out_size);
+
     QString base64 = encryptedArg.toBase64(QByteArray::Base64Option::Base64UrlEncoding | QByteArray::Base64Option::OmitTrailingEquals);
     char checksum = GetChecksum(key);
 
@@ -39,12 +50,19 @@ inline QString decryptGameArg(uint32_t tickCount, QString sqexString) {
     char buffer[9] = {};
     sprintf(buffer, "%08x", key);
 
-    Blowfish session(QByteArray(buffer, 8));
+    Blowfish* session = physis_blowfish_initialize(reinterpret_cast<uint8_t *>(buffer), 9);
 
     QStringRef base64String(&sqexString, 12, sqexString.length() - 5 - 12);
     QByteArray base64Decoded = QByteArray::fromBase64(base64String.toUtf8(), QByteArray::Base64Option::Base64UrlEncoding | QByteArray::Base64Option::OmitTrailingEquals);
 
-    auto decrypted = session.Decrypt(base64Decoded).trimmed();
+    uint8_t* out_data = nullptr;
+    uint32_t out_size = 0;
+
+    physis_blowfish_decrypt(session,
+                            reinterpret_cast<uint8_t*>(base64Decoded.data()), base64Decoded.size(), &out_data, &out_size);
+
+    QByteArray decrypted = QByteArray::fromRawData(
+        reinterpret_cast<const char*>(out_data), out_size).trimmed();
 
     return decrypted;
 }
