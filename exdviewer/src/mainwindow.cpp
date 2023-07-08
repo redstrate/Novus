31f7e5733e90ec042f9d2af1807344560efc4d5f
Joshua Goins <josh@redstrate.com>
2023-07-08 10:33:12 -0400

    Add about window to exdviewer and explorer as well

--- exdviewer/src/mainwindow.cpp
+++ exdviewer/src/mainwindow.cpp
@@ -5,11 +5,38 @@
 #include <fmt/core.h>
 #include <QListWidget>
 #include <physis.hpp>
+#include <QDesktopServices>
+#include <QAction>
+#include <QMenuBar>
+#include <QUrl>
+#include <QApplication>
 
 #include "exdpart.h"
+#include "aboutwindow.h"
 
 MainWindow::MainWindow(GameData* data) : data(data) {
     setWindowTitle("exdviewer");
+
+    auto helpMenu = menuBar()->addMenu("Help");
+
+    auto donateAction = helpMenu->addAction("Donate");
+    connect(donateAction, &QAction::triggered, this, [] {
+        QDesktopServices::openUrl(QUrl("https://redstrate.com/fund"));
+    });
+    donateAction->setIcon(QIcon::fromTheme("help-donate"));
+
+    helpMenu->addSeparator();
+
+    auto aboutNovusAction = helpMenu->addAction("About exdviewer");
+    aboutNovusAction->setIcon(QIcon::fromTheme("help-about"));
+    connect(aboutNovusAction, &QAction::triggered, this, [this] {
+        auto window = new AboutWindow(this);
+        window->show();
+    });
+
+    auto aboutQtAction = helpMenu->addAction("About Qt");
+    aboutQtAction->setIcon(QIcon(":/qt-project.org/qmessagebox/images/qtlogo-64.png"));
+    connect(aboutQtAction, &QAction::triggered, QApplication::instance(), &QApplication::aboutQt);
 
     auto dummyWidget = new QWidget();
     setCentralWidget(dummyWidget);
--- explorer/src/mainwindow.cpp
+++ explorer/src/mainwindow.cpp
@@ -1,14 +1,42 @@
 #include "mainwindow.h"
-#include "filetreewindow.h"
-#include "filepropertieswindow.h"
 
+#include <QDesktopServices>
+#include <QAction>
+#include <QMenuBar>
+#include <QUrl>
+#include <QApplication>
 #include <QHBoxLayout>
 #include <QTableWidget>
 #include <QTreeWidget>
 #include <QDebug>
 
+#include "filetreewindow.h"
+#include "filepropertieswindow.h"
+#include "aboutwindow.h"
+
 MainWindow::MainWindow(GameData* data) : data(data) {
     setWindowTitle("explorer");
+
+    auto helpMenu = menuBar()->addMenu("Help");
+
+    auto donateAction = helpMenu->addAction("Donate");
+    connect(donateAction, &QAction::triggered, this, [] {
+        QDesktopServices::openUrl(QUrl("https://redstrate.com/fund"));
+    });
+    donateAction->setIcon(QIcon::fromTheme("help-donate"));
+
+    helpMenu->addSeparator();
+
+    auto aboutNovusAction = helpMenu->addAction("About explorer");
+    aboutNovusAction->setIcon(QIcon::fromTheme("help-about"));
+    connect(aboutNovusAction, &QAction::triggered, this, [this] {
+        auto window = new AboutWindow(this);
+        window->show();
+    });
+
+    auto aboutQtAction = helpMenu->addAction("About Qt");
+    aboutQtAction->setIcon(QIcon(":/qt-project.org/qmessagebox/images/qtlogo-64.png"));
+    connect(aboutQtAction, &QAction::triggered, QApplication::instance(), &QApplication::aboutQt);
 
     mdiArea = new QMdiArea();
     setCentralWidget(mdiArea);
