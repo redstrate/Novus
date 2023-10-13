678304f583fd5221630a805b81a336ec963ff921
Joshua Goins <josh@redstrate.com>
2023-10-12 21:43:49 -0400

    texpart: Add missing files

---
+++ parts/tex/texpart.cpp
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2023 Joshua Goins <josh@redstrate.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "texpart.h"
+
+#include <QVBoxLayout>
+#include <physis.hpp>
+
+TexPart::TexPart(GameData *data)
+    : data(data)
+{
+    auto layout = new QVBoxLayout();
+    setLayout(layout);
+
+    m_label = new ImageLabel();
+    layout->addWidget(m_label);
+}
+
+void TexPart::load(physis_Buffer file)
+{
+    auto tex = physis_texture_parse(file);
+
+    QImage image(tex.rgba, tex.width, tex.height, QImage::Format_RGBA8888);
+    m_label->setQPixmap(QPixmap::fromImage(image));
+}
---
+++ parts/tex/texpart.h
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: 2023 Joshua Goins <josh@redstrate.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#pragma once
+
+#include <QWidget>
+#include <physis.hpp>
+
+#include "imagelabel.h"
+
+class TexPart : public QWidget
+{
+public:
+    explicit TexPart(GameData *data);
+
+    void load(physis_Buffer file);
+
+private:
+    GameData *data = nullptr;
+
+    ImageLabel *m_label = nullptr;
+};
