4c3a9795d2da5810a2bc0a7a6007a13119008df3
Joshua Goins <josh@redstrate.com>
2023-10-12 23:44:43 -0400

    argcracker: Reformat code, misc cleanup

--- argcracker/src/tickcount.cpp
+++ argcracker/src/tickcount.cpp
@@ -4,22 +4,23 @@
 #include <cstdint>
 
 #if defined(MACOS)
-    #include <mach/mach_time.h>
-    #include <sys/sysctl.h>
+#include <mach/mach_time.h>
+#include <sys/sysctl.h>
 #endif
 
 #if defined(WIN32)
-    #include <windows.h>
+#include <windows.h>
 #endif
 
 #if defined(MACOS)
 // from XIV-on-Mac, I think it changed in recent Wine versions?
-uint32_t TickCount() {
+uint32_t TickCount()
+{
     struct mach_timebase_info timebase;
     mach_timebase_info(&timebase);
 
     auto machtime = mach_continuous_time();
-    auto numer = uint64_t (timebase.numer);
+    auto numer = uint64_t(timebase.numer);
     auto denom = uint64_t(timebase.denom);
     auto monotonic_time = machtime * numer / denom / 100;
     return monotonic_time / 10000;
@@ -29,7 +30,8 @@ uint32_t TickCount() {
 #if defined(LINUX)
 #include <ctime>
 
-uint32_t TickCount() {
+uint32_t TickCount()
+{
     struct timespec ts;
 
     clock_gettime(CLOCK_MONOTONIC, &ts);
@@ -39,7 +41,8 @@ uint32_t TickCount() {
 #endif
 
 #if defined(WIN32)
-uint32_t TickCount() {
+uint32_t TickCount()
+{
     return GetTickCount();
 }
 #endif
