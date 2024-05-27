f15049d6aa3f16d818dae6c6799d5e51619e86ad
Joshua Goins <josh@redstrate.com>
2024-05-27 13:15:50 -0400

    Map Editor: Begin adding support for materials

--- apps/mapeditor/src/mapview.cpp
+++ apps/mapeditor/src/mapview.cpp
@@ -38,11 +38,19 @@ void MapView::addTerrain(QString basePath, physis_Terrain terrain)
         auto plateMdlFile = physis_gamedata_extract_file(data, mdlPathStd.c_str());
         auto plateMdl = physis_mdl_parse(plateMdlFile);
         if (plateMdl.p_ptr != nullptr) {
+            std::vector<physis_Material> materials;
+            for (uint32_t j = 0; j < plateMdl.num_material_names; j++) {
+                const char *material_name = plateMdl.material_names[j];
+
+                auto mat = physis_material_parse(cache.lookupFile(QLatin1String(material_name)));
+                materials.push_back(mat);
+            }
+
             mdlPart->addModel(plateMdl,
                               false,
                               glm::vec3(terrain.plates[i].position[0], 0.0f, terrain.plates[i].position[1]),
                               QStringLiteral("terapart%1").arg(i),
-                              {},
+                              materials,
                               0);
         }
     }
