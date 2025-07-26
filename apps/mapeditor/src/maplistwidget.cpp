747621f0feae6691556040aa4908792c7abed280
Joshua Goins <josh@redstrate.com>
2025-07-25 22:52:07 -0400

    Use the TerritoryType sheet for listing maps instead of Map
    This means we can list territories that don't have maps, and it just
    makes more ense.

--- apps/mapeditor/src/maplistwidget.cpp
+++ apps/mapeditor/src/maplistwidget.cpp
@@ -34,51 +34,42 @@ MapListWidget::MapListWidget(SqPackResource *data, QWidget *parent)
     auto originalModel = new QStandardItemModel();
     searchModel->setSourceModel(originalModel);
 
-    auto exh = physis_parse_excel_sheet_header(physis_gamedata_extract_file(data, "exd/Map.exh"));
     auto nameExh = physis_parse_excel_sheet_header(physis_gamedata_extract_file(data, "exd/PlaceName.exh"));
     auto territoryExh = physis_parse_excel_sheet_header(physis_gamedata_extract_file(data, "exd/TerritoryType.exh"));
 
     // Only one page, it seems?
-    auto exd = physis_gamedata_read_excel_sheet(data, "Map", exh, Language::None, 0);
     auto nameExd = physis_gamedata_read_excel_sheet(data, "PlaceName", nameExh, Language::English, 0);
     auto territoryExd = physis_gamedata_read_excel_sheet(data, "TerritoryType", territoryExh, Language::None, 0);
 
-    for (uint32_t i = 0; i < exh->row_count; i++) {
-        auto rows = physis_exd_read_row(&exd, i); // TODO: free, use all rows
-
-        const char *id = rows.row_data[0].column_data[6].string._0;
-
-        const uint16_t territoryTypeKey = rows.row_data[0].column_data[15].u_int16._0;
-        if (territoryTypeKey > 0 && territoryTypeKey < territoryExh->row_count) {
-            auto territoryExdRow = physis_exd_read_row(&territoryExd, territoryTypeKey); // TODO: free, use all rows
-            if (territoryExdRow.row_count != 0) {
-                const char *bg = territoryExdRow.row_data[0].column_data[1].string._0;
-                if (strlen(bg) == 0) {
-                    continue;
-                }
-
-                int placeRegionKey = territoryExdRow.row_data[0].column_data[3].u_int16._0;
-                auto regionExdRow = physis_exd_read_row(&nameExd, placeRegionKey); // TODO: free, use all rows
-                const char *placeRegion = regionExdRow.row_data[0].column_data[0].string._0;
-
-                int placeZoneKey = territoryExdRow.row_data[0].column_data[4].u_int16._0;
-                auto zoneExdRow = physis_exd_read_row(&nameExd, placeZoneKey); // TODO: free, use all rows
-                const char *placeZone = zoneExdRow.row_data[0].column_data[0].string._0;
-
-                int placeNameKey = territoryExdRow.row_data[0].column_data[5].u_int16._0;
-                auto nameExdRow = physis_exd_read_row(&nameExd, placeNameKey); // TODO: free, use all rows
-                const char *placeName = nameExdRow.row_data[0].column_data[0].string._0;
-
-                QStandardItem *item = new QStandardItem();
-                item->setData(QString::fromStdString(bg));
-                item->setText(QStringLiteral("%1 (%2, %3, %4)")
-                                  .arg(QString::fromStdString(id),
-                                       QString::fromStdString(placeRegion),
-                                       QString::fromStdString(placeZone),
-                                       QString::fromStdString(placeName)));
-
-                originalModel->insertRow(originalModel->rowCount(), item);
+    for (uint32_t i = 0; i < territoryExh->row_count; i++) {
+        auto territoryExdRow = physis_exd_read_row(&territoryExd, i); // TODO: free, use all rows
+        if (territoryExdRow.row_count != 0) {
+            const char *bg = territoryExdRow.row_data[0].column_data[1].string._0;
+            if (strlen(bg) == 0) {
+                continue;
             }
+
+            int placeRegionKey = territoryExdRow.row_data[0].column_data[3].u_int16._0;
+            auto regionExdRow = physis_exd_read_row(&nameExd, placeRegionKey); // TODO: free, use all rows
+            const char *placeRegion = regionExdRow.row_data[0].column_data[0].string._0;
+
+            int placeZoneKey = territoryExdRow.row_data[0].column_data[4].u_int16._0;
+            auto zoneExdRow = physis_exd_read_row(&nameExd, placeZoneKey); // TODO: free, use all rows
+            const char *placeZone = zoneExdRow.row_data[0].column_data[0].string._0;
+
+            int placeNameKey = territoryExdRow.row_data[0].column_data[5].u_int16._0;
+            auto nameExdRow = physis_exd_read_row(&nameExd, placeNameKey); // TODO: free, use all rows
+            const char *placeName = nameExdRow.row_data[0].column_data[0].string._0;
+
+            QStandardItem *item = new QStandardItem();
+            item->setData(QString::fromStdString(bg));
+            item->setText(QStringLiteral("%1 (%2, %3, %4)")
+                              .arg(QString::fromStdString(bg),
+                                   QString::fromStdString(placeRegion),
+                                   QString::fromStdString(placeZone),
+                                   QString::fromStdString(placeName)));
+
+            originalModel->insertRow(originalModel->rowCount(), item);
         }
     }
 
