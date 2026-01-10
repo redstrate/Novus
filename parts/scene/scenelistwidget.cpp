2d768fd1d26f8e1f3f879121a0fda0283b995861
Joshua Goins <josh@redstrate.com>
2026-01-09 22:20:30 -0500

    Fix column sizes for scene list widget

--- parts/scene/scenelistwidget.cpp
+++ parts/scene/scenelistwidget.cpp
@@ -39,8 +39,8 @@ SceneListWidget::SceneListWidget(SceneState *appState, QWidget *parent)
     treeWidget = new QTreeView();
     treeWidget->setWhatsThis(i18nc("@info:whatsthis", "A list of objects on this map."));
     treeWidget->setModel(searchModel);
-    treeWidget->header()->setSectionResizeMode(0, QHeaderView::ResizeToContents);
-    treeWidget->header()->setSectionResizeMode(1, QHeaderView::Fixed);
+    treeWidget->header()->setStretchLastSection(false);
+    treeWidget->header()->setSectionsMovable(false);
     connect(treeWidget, &QTreeView::activated, this, [this, searchModel](const QModelIndex &index) {
         auto originalIndex = searchModel->mapToSource(index);
         m_appState->selectedObject = m_objectListModel->objectAt(originalIndex);
@@ -50,5 +50,10 @@ SceneListWidget::SceneListWidget(SceneState *appState, QWidget *parent)
     layout->addWidget(treeWidget);
 
     m_objectListModel = new SceneListModel(m_appState, treeWidget);
+    connect(m_objectListModel, &SceneListModel::modelReset, this, [this] {
+        // This has to be called *here*, for some reason...
+        treeWidget->header()->setSectionResizeMode(0, QHeaderView::Stretch);
+        treeWidget->header()->setSectionResizeMode(1, QHeaderView::ResizeToContents);
+    });
     searchModel->setSourceModel(m_objectListModel);
 }
